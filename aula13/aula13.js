58f5405108a09c32f9f6a06ee5ade3250c866032
Fabriciogabriel13 <fabricio.gabriel@academico.ifpb.edu.br>
2023-10-02 23:04:47 -0300

    exer1

---
+++ aula13/aula13.js
@@ -0,0 +1,13 @@
+//repetição com "while" antes
+var count = 1
+while (count <= 10){
+    console.log(`contagem : ${count}`)
+    count = count + 1
+}
+//repetição com o "do" antes
+/*
+var count = 1
+do{
+    console.log(`contagem: ${count}`)
+    count++
+} while (count <= 10)*/
---
+++ aula14/aula14.js
@@ -0,0 +1,10 @@
+/*var c = 1;
+while (c <= 10){
+    console.log(`mostre os numero:${c}`)
+    c = c +1
+}*/
+console.log('vai começar...')
+for (var c = 1; c <= 10; c = c+1){
+    console.log(`${c}`)
+}
+console.log('fim!)
